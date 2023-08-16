3e852cc528b2fa10fdb51f0665aacf5b17f17228
Katzuuu <dodookormos@gmail.com>
2023-08-16 22:15:27 +0200

    last commit

--- app/product/[...id]/page.tsx
+++ app/product/[...id]/page.tsx
@@ -58,55 +58,57 @@ const detailPage = () => {
     });
   return (
     <>
-      {currentProduct && (
-        <motion.div
-          variants={container2}
-          initial="initial"
-          animate="show"
-          className="padding overflow-hidden w-full md:w-2/3 lg:w-full mx-auto my-auto py-[30px] flex lg:flex-row flex-col items-center justify-center gap-8 md:gap-[60px] lg:gap-[100px] sm:h-[200%] md:h-[calc(100vh-78px)] lg:h-[calc(100vh-88px)]"
-        >
-          <ImagePreview images={currentProduct.images} />
-          <div className="flex flex-col gap-6 justify-center pr-0 xl:pr-32">
-            <div className="overflow-hidden pb-1">
-              <motion.h2
-                variants={header}
-                className={`${poppins.className} text-2xl lg:text-4xl`}
-              >
-                {currentProduct.title}
-              </motion.h2>
-            </div>
-            <motion.p className="text-sm lg:text-base" variants={show2}>
-              {currentProduct.description}
-            </motion.p>
-            <div className="flex gap-6 items-center">
-              <motion.p
-                variants={slideRight}
-                className={`${anton.className} tracking-wider text-[15px] lg:text-[30px] font-bold`}
-              >
-                {currentProduct.price}€
-              </motion.p>
-              <motion.button
-                variants={scaleDown}
-                onClick={() => {
-                  addProduct(currentProduct._id);
-                  message(`${currentProduct.title} is in cart!`);
-                }}
-                className="hover:bg-customPurple hover:text-white transition-colors btn bg-transparent text-customPurple outline outline-1 outline-customPurple inline-flex items-center gap-2"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
+      <div className="padding overflow-hidden w-full md:w-2/3 lg:w-full mx-auto my-auto py-[30px] flex lg:flex-row flex-col items-center justify-center gap-8 md:gap-[60px] lg:gap-[100px] h-full pt-0 sm:h-[200%] md:h-[calc(100vh-78px)] lg:h-[calc(100vh-88px)]">
+        {currentProduct && (
+          <motion.div
+            variants={container2}
+            initial="initial"
+            animate="show"
+            className="w-full flex lg:flex-row flex-col items-center justify-center gap-8 md:gap-[60px] lg:gap-[100px] h-full"
+          >
+            <ImagePreview images={currentProduct.images} />
+            <div className="flex flex-col gap-6 justify-center pr-0 xl:pr-32">
+              <div className="overflow-hidden pb-1">
+                <motion.h2
+                  variants={header}
+                  className={`${poppins.className} text-2xl lg:text-4xl`}
                 >
-                  <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-                </svg>
-                <span>Add to cart</span>
-              </motion.button>
+                  {currentProduct.title}
+                </motion.h2>
+              </div>
+              <motion.p className="text-sm lg:text-base" variants={show2}>
+                {currentProduct.description}
+              </motion.p>
+              <div className="flex gap-6 items-center">
+                <motion.p
+                  variants={slideRight}
+                  className={`${anton.className} tracking-wider text-[15px] lg:text-[30px] font-bold`}
+                >
+                  {currentProduct.price}€
+                </motion.p>
+                <motion.button
+                  variants={scaleDown}
+                  onClick={() => {
+                    addProduct(currentProduct._id);
+                    message(`${currentProduct.title} is in cart!`);
+                  }}
+                  className="hover:bg-customPurple hover:text-white transition-colors btn bg-transparent text-customPurple outline outline-1 outline-customPurple inline-flex items-center gap-2"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                  </svg>
+                  <span>Add to cart</span>
+                </motion.button>
+              </div>
             </div>
-          </div>
-        </motion.div>
-      )}
+          </motion.div>
+        )}
+      </div>
       <CommentSection productId={currentProduct?._id} font={anton.className} />
     </>
   );
